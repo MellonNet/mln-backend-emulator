ed577ebeb0d54d57c645ee4cb9d08578d74e3140
lcdr <lcdr@users.noreply.github.com>
2018-08-31 14:12:18 +0200

    Change the starting items from hardcoded IDs to name lookups where possible

--- mln/signals.py
+++ mln/signals.py
@@ -4,29 +4,43 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models.dynamic import Profile
+from .models.static import ItemInfo
+
+def get_id(name):
+	return ItemInfo.objects.get(name=name).id
+
+START_ITEMS = [
+	# Items
+	(get_id("Red LEGO Brick"), 50),
+	# Blueprints
+	(get_id("Apple Blueprint"), 1),
+	(get_id("Trade Module Blueprint"), 1),
+	# Modules
+	(get_id("Factory Module"), 2),
+	(get_id("Gallery Module"), 2),
+	(get_id("LEGO Tree Module"), 1),
+	(get_id("Soundtrack Module"), 1),
+	(43620, 2), # Sticker Module width 3
+	(43887, 3), # Sticker Module width 1
+	(43888, 3), # Sticker Module width 2
+	# Stickers
+	(get_id("Fire Truck"), 1),
+	(45059, 1), # Creator House
+	(get_id("Police Officer"), 1),
+	(get_id("Pridak"), 1),
+	(get_id("Creator Buggy"), 1),
+	# Loops
+	(get_id("Electric Guitar 1"), 1),
+	(get_id("Hip Hop Drums with Clapping"), 1),
+	(get_id("Rock Drums 1"), 1),
+	(get_id("Simple Bass 1"), 1),
+	(get_id("Simple Bass 2"), 1),
+]
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	"""Add a profile for user data, as well as MLN's starting items, when a user is created."""
 	if created:
 		profile = Profile.objects.create(user=instance)
-		profile.add_inv_item(45056)
-		profile.add_inv_item(45059)
-		profile.add_inv_item(45077)
-		profile.add_inv_item(43573)
-		profile.add_inv_item(43586)
-		profile.add_inv_item(43593)
-		profile.add_inv_item(43596)
-		profile.add_inv_item(43597)
-		profile.add_inv_item(43598)
-		profile.add_inv_item(43599)
-		profile.add_inv_item(43600)
-		profile.add_inv_item(53602)
-		profile.add_inv_item(43620, qty=2)
-		profile.add_inv_item(43876)
-		profile.add_inv_item(43887, qty=3)
-		profile.add_inv_item(43888, qty=3)
-		profile.add_inv_item(45173)
-		profile.add_inv_item(43907, qty=2)
-		profile.add_inv_item(43911, qty=2)
-		profile.add_inv_item(43449, qty=50)
+		for item_id, qty in START_ITEMS:
+			profile.add_inv_item(item_id, qty)
