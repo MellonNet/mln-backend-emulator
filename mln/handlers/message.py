854858abd76a28973b6deefd78bdb89b83632b08
lcdr <lcdr@localhost>
2018-09-13 12:28:06 +0200

    Move the message related logic to a dedicated services file

--- mln/handlers/message.py
+++ mln/handlers/message.py
@@ -1,38 +1,28 @@
 """Mail functionality handlers."""
-from ..models.dynamic import Message
+from ..services.message import create_attachment, delete_message, detach_attachments, easy_reply, open_message, send_message
 
 def handle_message_delete(user, request):
-	"""Delete a message from the user's inbox."""
-	message = handle_message_detach(user, request)
-	message.delete()
+	message_id = int(request.get("messageID"))
+	delete_message(user, message_id)
 
 def handle_message_detach(user, request):
-	"""Collect an attachment from a message, place it in the user's inventory."""
 	message_id = int(request.get("messageID"))
-	message = Message.objects.get(id=message_id)
-	for attachment in message.attachments.all():
-		user.profile.add_inv_item(attachment.item_id, attachment.qty)
-	message.attachments.all().delete()
-	return message
+	detach_attachments(user, message_id)
 
 def handle_message_easy_reply(user, request):
-	"""Send a reply from a list of suggested replies."""
 	recipient_id = int(request.get("recipientID"))
 	org_body_id = int(request.get("orgBodyID"))
 	body_id = int(request.get("bodyID"))
-	return Message.objects.create(sender=user, recipient_id=recipient_id, body_id=body_id, reply_body_id=org_body_id)
+	easy_reply(user, recipient_id, org_body_id, body_id)
 
 def handle_message_easy_reply_with_attachments(user, request):
 	"""Like easy reply, but also attach items."""
 	message = handle_message_easy_reply(user, request)
-	create_attachment(message, request)
+	handle_create_attachment(request, message)
 
 def handle_message_get(user, request):
-	"""Open a message to read. Return sender avatar and attachment info. Mark the message as read."""
 	message_id = int(request.get("messageID"))
-	message = Message.objects.get(id=message_id)
-	message.is_read = True
-	message.save()
+	message = open_message(user, message_id)
 	return {"message": message}
 
 def handle_message_list(user, request):
@@ -41,19 +31,16 @@ def handle_message_list(user, request):
 	return {"messages": messages}
 
 def handle_message_send(user, request):
-	"""Send a message to someone."""
 	recipient_id = int(request.get("recipientID"))
 	body_id = int(request.get("bodyID"))
-	return Message.objects.create(sender=user, recipient_id=recipient_id, body_id=body_id)
+	return send_message(user, recipient_id, body_id)
 
 def handle_message_send_with_attachment(user, request):
 	"""Send a message with attachment."""
 	message = handle_message_send(user, request)
-	create_attachment(message, request)
+	handle_create_attachment(request, message)
 
-def create_attachment(message, request):
-	"""Remove items from the user's inventory and attach it to the message."""
-	attachment_item_id = int(request.get("itemID"))
-	attachment_qty = int(request.get("qty"))
-	message.sender.profile.remove_inv_item(attachment_item_id, attachment_qty)
-	message.attachments.create(item_id=attachment_item_id, qty=attachment_qty, message=message)
+def handle_create_attachment(request, message):
+	item_id = int(request.get("itemID"))
+	qty = int(request.get("qty"))
+	create_attachment(message, item_id, qty)
---
+++ mln/services/message.py
@@ -0,0 +1,57 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from ..models.dynamic import FriendshipStatus, Message
+
+def _check_recipient(user, recipient_id):
+	if not user.profile.is_networker:
+		if not _is_friend(user, recipient_id):
+			raise RuntimeError("User with ID %i is not a friend of %s" % (recipient_id, user))
+
+def _get_message(user, message_id):
+	message = Message.objects.get(id=message_id)
+	if message.recipient != user:
+		raise RuntimeError("%s is not addressed to user %s" % (message, user))
+	return message
+
+def _is_friend(user, friend_id):
+	return user.profile.outgoing_friendships.filter(to_profile_id=friend_id, status=FriendshipStatus.FRIEND.value).exists() or user.profile.incoming_friendships.filter(from_profile_id=friend_id, status=FriendshipStatus.FRIEND.value).exists()
+
+def create_attachment(message, item_id, qty):
+	"""Remove items from the user's inventory and attach it to the message."""
+	message.sender.profile.remove_inv_item(item_id, qty)
+	message.attachments.create(item_id=item_id, qty=qty)
+
+def delete_message(user, message_id):
+	"""Delete a message from the user's inbox, detaching any attachments."""
+	message = detach_attachments(user, message_id)
+	message.delete()
+
+def detach_attachments(user, message_id):
+	"""Remove attachments from a message and place them in the user's inventory."""
+	message = _get_message(user, message_id)
+	for attachment in message.attachments.all():
+		user.profile.add_inv_item(attachment.item_id, attachment.qty)
+	message.attachments.all().delete()
+	return message
+
+def easy_reply(user, recipient_id, org_body_id, body_id):
+	"""Send a reply from a list of suggested replies."""
+	_check_recipient(user, recipient_id)
+	if not user.messages.filter(body_id=org_body_id, sender_id=recipient_id).exists():
+		raise RuntimeError("User %s does not have a message with body ID %i from user with ID %i to reply to" % (user, org_body_id, recipient_id))
+	body = MessageBody.objects.get(id=org_body_id)
+	if not body.filter(id=body_id).exists():
+		raise RuntimeError("Message body with ID %i is not an easy reply of %s" % (body_id, body))
+	Message.objects.create(sender=user, recipient_id=recipient_id, body_id=body_id, reply_body_id=org_body_id)
+
+def open_message(user, message_id):
+	"""Get a message and mark it as read."""
+	message = _get_message(user, message_id)
+	message.is_read = True
+	message.save()
+	return message
+
+def send_message(user, recipient_id, body_id):
+	"""Send a message to someone."""
+	_check_recipient(user, recipient_id)
+	return Message.objects.create(sender=user, recipient_id=recipient_id, body_id=body_id)
