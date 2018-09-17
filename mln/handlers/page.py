4f4dbf05ba925913d3721e67c148679a2a3f66a0
lcdr <lcdr@users.noreply.github.com>
2018-09-17 11:41:31 +0200

    Move the page related handlers to a dedicated services file

--- mln/handlers/page.py
+++ mln/handlers/page.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 
 from ..models.dynamic import get_or_none, Friendship, FriendshipStatus
 from ..models.static import ItemType
+from ..services.page import page_save_layout, page_save_options
 
 def handle_page_get_new(viewing_user, request):
 	page_owner_name = request.get("pageOwner")
@@ -65,49 +66,30 @@ def handle_page_get_new(viewing_user, request):
 
 	return context
 
-def get_or_create_module(user, elem):
-	instance_id = elem.get("instanceID")
-	if instance_id == "00000000-0000-0000-0000-000000000000":
-		item_id = int(elem.get("itemID"))
-		if not user.profile.is_networker:
-			user.profile.remove_inv_item(item_id)
-		module = user.modules.create(item_id=item_id, pos_x=0, pos_y=0)
-	else:
-		module = user.modules.get(id=int(instance_id))
-	return module
-
 def handle_page_save_layout(user, request):
-	present_ids = []
+	modules = []
 	for module_elem in request.findall("result/item"):
+		instance_id = module_elem.get("instanceID")
+		if instance_id == "00000000-0000-0000-0000-000000000000":
+			instance_id = None
+		else:
+			instance_id = int(instance_id)
+		item_id = int(module_elem.get("itemID"))
 		details = module_elem.find("details")
 		pos_x = int(details.get("posx"))
 		pos_y = int(details.get("posy"))
-
-		module = get_or_create_module(user, module_elem)
-		module.pos_x = pos_x
-		module.pos_y = pos_y
-		module.save()
-		present_ids.append(module.id)
-
-	# if a module is not in the supplied list of modules it was removed and should be deleted
-	removed_modules = user.modules.exclude(id__in=present_ids)
-	if not user.profile.is_networker:
-		for module in removed_modules.all():
-			if module.is_setup:
-				module.teardown()
-			user.profile.add_inv_item(module.item_id)
-	removed_modules.delete()
-
-	return {}
+		modules.append((instance_id, item_id, pos_x, pos_y))
+	page_save_layout(user, modules)
 
 def handle_page_save_options(user, request):
 	settings = request.find("result/settings/color")
 	if settings.get("skinID") == "undefined":
-		user.profile.page_skin = None
+		skin_id = None
 	else:
-		user.profile.page_skin_id = int(settings.get("skinID"))
-
-	color_id = int(settings.get("colorID"))
-	user.profile.page_color_id = color_id
-	user.profile.page_column_color_id = int(settings.get("columnColorID"))
-	user.profile.save()
+		skin_id = int(settings.get("skinID"))
+	if settings.get("colorID") == "undefined":
+		color_id = None
+	else:
+		color_id = int(settings.get("colorID"))
+	column_color_id = int(settings.get("columnColorID"))
+	page_save_options(user, skin_id, color_id, column_color_id)
