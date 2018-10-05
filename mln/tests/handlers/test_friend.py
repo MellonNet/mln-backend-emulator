712e5dd01d5fe45148a526853a7125ce9b682480
lcdr <lcdr@users.noreply.github.com>
2018-10-05 13:06:16 +0200

    Add tests for friend related handlers

---
+++ mln/tests/handlers/test_friend.py
@@ -0,0 +1,30 @@
+from mln.models.dynamic import FriendshipStatus
+from mln.tests.handlers.handler_testcase import req_resp
+from mln.tests.services.test_friend import blocked_friends, friends
+from mln.tests.setup_testcase import requires, setup, TestCase
+from mln.tests.test_profile import two_users
+
+@setup
+@requires(two_users)
+def pending_friends_other_way(self):
+	self.other_user.profile.outgoing_friendships.create(to_profile=self.user.profile, status=FriendshipStatus.PENDING)
+
+class FriendSendInvitationTest(TestCase, metaclass=req_resp):
+	SETUP = two_users,
+	DIR = "friend"
+	VOID_TESTS = "friend_send_invitation",
+
+class FriendProcessInvitationTest(TestCase, metaclass=req_resp):
+	SETUP = pending_friends_other_way,
+	DIR = "friend"
+	VOID_TESTS = "friend_process_invitation",
+
+class FriendTest(TestCase, metaclass=req_resp):
+	SETUP = friends,
+	DIR = "friend"
+	VOID_TESTS = "friend_remove_member", "friend_process_blocking_block"
+
+class FriendProcessBlockingUnblockTest(TestCase, metaclass=req_resp):
+	SETUP = blocked_friends,
+	DIR = "friend"
+	VOID_TESTS = "friend_process_blocking_unblock",
