1c9aac7077219972021361c9a16bf43156e161b7
Levi Lesches <levilesches@gmail.com>
2025-07-14 23:47:09 -0400

    Implemented the robot chronicles (#45)
    Make the following POST request: 
    
    ```js
    POST /api/robot-chronicles/award
    {
      "api_token": string,
      "access_token": string,
      "award": int,   // 1-5, inclusive
    }
    ```

--- mln/models/static/static.py
+++ mln/models/static/static.py
@@ -157,6 +157,8 @@ class MessageBodyType(Enum):
 	INTEGRATION = auto()
 	UNPUBLISHED = auto()
 	OTHER = auto()
+	COAST_GUARD = auto()
+	ROBOT_CHRONICLES = auto()
 
 class MessageBody(models.Model):
 	"""
@@ -377,6 +379,14 @@ class CoastGuardMessage(models.Model):
 	template = models.ForeignKey(MessageTemplate, related_name="+", on_delete=models.CASCADE)
 	networker = models.ForeignKey(User, related_name="+", on_delete=models.CASCADE, limit_choices_to={"profile__is_networker": True})
 	rank = models.IntegerField()
+
+	def __str__(self):
+		return f"Rank {self.rank} message"
+
+class RobotChroniclesMessage(models.Model):
+	template = models.ForeignKey(MessageTemplate, related_name="+", on_delete=models.CASCADE)
+	networker = models.ForeignKey(User, related_name="+", on_delete=models.CASCADE, limit_choices_to={"profile__is_networker": True})
+	award = models.IntegerField()
 
 	def __str__(self):
 		return f"Rank {self.rank} message"
