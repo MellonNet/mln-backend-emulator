0ea90b7d4efc3fe8d324d087d4dd9d40b05f615a
Levi Lesches <levilesches@gmail.com>
2025-09-05 04:19:46 -0400

    Set DEBUG=false

--- mlnserver/settings.py
+++ mlnserver/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "q67!2f=-w+(+%i7txzukqg)ik(esyizuu$n^-ed5q8jeqy2mr="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   "127.0.0.1",
